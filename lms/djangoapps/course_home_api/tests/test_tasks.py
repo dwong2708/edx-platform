6bbf5b6fa28ad8648dbc852ccc4d012cdbf99a64
Justin Hynes <jhynes@2u.com>
2025-04-09 18:44:25 +0000

    fix: quality

--- lms/djangoapps/course_home_api/tests/test_tasks.py
+++ lms/djangoapps/course_home_api/tests/test_tasks.py
@@ -42,7 +42,7 @@ class CalculateCompletionTaskTests(ModuleStoreTestCase):
             "locked_count": 1,
         }
 
-        expected_data =  {
+        expected_data = {
             "lms_user_id": self.user.id,
             "course_id": self.course_run_key_string,
             "enrollment_mode": "verified",
